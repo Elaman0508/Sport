c6c208295c495e878c189b7838b41250cb03bbc3
камила <taalakamilaibekkuzu@gmail.com>
2024-10-14 16:37:53 +0600

    aa

--- core/client_account/admin.py
+++ core/client_account/admin.py
@@ -21,3 +21,27 @@ class ScheduleAdmin(admin.ModelAdmin):
 
     sport.short_description = 'Спорт'
 
+class Payment1Admin(admin.ModelAdmin):
+    list_display = ('user', 'schedule', 'amount', 'paid', 'payment_date')  # Поля, отображаемые в списке
+    list_filter = ('paid', 'payment_date')  # Фильтры для поиска
+    search_fields = ('user__username', 'amount')  # Поля для поиска
+    ordering = ('-payment_date',)  # Порядок сортировки по умолчанию
+
+    # Настройка форм для редактирования
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'schedule', 'amount', 'paid')
+        }),
+        # Убираем payment_date из fieldsets
+        # ('Дата платежа', {
+        #     'fields': ('payment_date',),
+        #     'classes': ('collapse',),  # Скрывает поле по умолчанию
+        # }),
+    )
+
+    # Не нужно добавлять payment_date в read-only fields, так как оно не редактируется
+    def get_readonly_fields(self, request, obj=None):
+        return ['payment_date'] if obj else []
+
+# Регистрация модели Payment1 с администратором
+admin.site.register(Payment1, Payment1Admin)
