35875424b897eb658bffd3deb6dc980e12df2f0f
камила <taalakamilaibekkuzu@gmail.com>
2024-10-23 16:11:06 +0600

    fghre

--- core/client_account/models.py
+++ core/client_account/models.py
@@ -62,11 +62,7 @@ class Payment1(models.Model):
         on_delete=models.CASCADE,
         verbose_name="Пользователь"
     )
-    sport = models.CharField(
-        max_length=20,
-        choices=SPORTS_CHOICES,
-        verbose_name="Спорт"
-    )
+    sport= models.CharField(verbose_name='Виды спорта', max_length=20, choices=SPORT_CHOICES)
     paid = models.BooleanField(
         default=False,
         verbose_name="Оплачено"
